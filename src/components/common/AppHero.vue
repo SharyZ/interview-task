7886e8f3692dacce9bfd6dc4cfb049c66694891a
SharyZ <onshariiit@gmail.com>
2021-02-12 19:31:12 +0500

    feat: create hero component

---
+++ src/components/common/AppHero.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="app-hero">
+    <h2>{{ title }}</h2>
+    <p>{{ subtitle }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app-hero",
+  props: {
+    title: {
+      type: String,
+      default: () => "",
+      required: true,
+    },
+    subtitle: {
+      type: String,
+      default: () => "",
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.app-hero {
+  text-align: center;
+  padding: 2.5rem 5rem;
+  background-image: linear-gradient(to right top, #22d3ee, #0369a1);
+  color: #f8fafc;
+}
+
+.app-hero h2 {
+  margin-bottom: 1rem;
+}
+
+.app-hero p {
+  margin-bottom: 1.5rem;
+}
+</style>
--- src/globalComponents.js
+++ src/globalComponents.js
@@ -2,5 +2,8 @@ import Vue from "vue";
 import AppContainer from "@/components/layout/AppContainer";
 import AppLayout from "@/components/layout/AppLayout";
 
+import AppHero from "@/components/common/AppHero";
+
 Vue.component(AppContainer.name, AppContainer);
 Vue.component(AppLayout.name, AppLayout);
+Vue.component(AppHero.name, AppHero);

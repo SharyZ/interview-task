903c005520548808397471caaf68075cc45291b0
SharyZ <onshariiit@gmail.com>
2021-02-12 22:50:58 +0500

    setup eCommerce module

--- src/store/modules/eCommerce/actions.js
+++ src/store/modules/eCommerce/actions.js
@@ -1,1 +1,16 @@
-export default {};
+import stockService from "@/services/stockService";
+
+export default {
+  getAllSunglasses({ commit }) {
+    stockService.getAllSunglasses().then((response) => {
+      const { status, data } = response;
+      if (status === 200) {
+        commit("setIsLoading", false);
+        commit("setSunglasses", data);
+      } else {
+        commit("setIsLoading", false);
+        commit("setIsError", true);
+      }
+    });
+  },
+};
--- src/store/modules/eCommerce/mutations.js
+++ src/store/modules/eCommerce/mutations.js
@@ -1,1 +1,11 @@
-export default {};
+export default {
+  setSunglasses(state, payload) {
+    state.sunglasses = payload;
+  },
+  setIsLoading(state, payload) {
+    state.isLoading = payload;
+  },
+  setIsError(state, payload) {
+    state.isError = payload;
+  },
+};

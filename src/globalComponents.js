9c4f9b93448b0df2b51bec515359eb9a53b5e507
SharyZ <onshariiit@gmail.com>
2021-02-12 21:13:54 +0500

    globally register components

--- src/globalComponents.js
+++ src/globalComponents.js
@@ -3,7 +3,11 @@ import AppContainer from "@/components/layout/AppContainer";
 import AppLayout from "@/components/layout/AppLayout";
 
 import AppHero from "@/components/common/AppHero";
+import UIButton from "@/components/common/UIButton";
+import UICard from "@/components/common/UICard";
 
 Vue.component(AppContainer.name, AppContainer);
 Vue.component(AppLayout.name, AppLayout);
 Vue.component(AppHero.name, AppHero);
+Vue.component(UIButton.name, UIButton);
+Vue.component(UICard.name, UICard);
